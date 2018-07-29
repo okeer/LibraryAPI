affffb521fb0b6a73cb00087135db477438f5296
Sergey Orlov <serhio.orlovsky@gmail.com>
2018-07-29 19:42:41 +0700

    Fix incorrect type for Books collection

--- Library.API/Models/AuthorCreationDto.cs
+++ Library.API/Models/AuthorCreationDto.cs
@@ -1,9 +1,14 @@
-﻿namespace Library.API.Models
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Library.API.Models
 {
     public class AuthorCreationDto
     {
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public ICollection<BookCreationDto> Books { get; set; } = new List<BookCreationDto>();
     }
 }
